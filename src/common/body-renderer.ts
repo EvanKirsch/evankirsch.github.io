e92ffbe7cdce29c4ae7053056605b2a666432002
Evan Kirsch <40045743+EvanKirsch@users.noreply.github.com>
2026-02-12 18:56:14 +0000

    Add building with vite

--- src/common/body-renderer.ts
+++ src/common/body-renderer.ts
@@ -1,3 +1,7 @@
 export class BodyRenderer {
 
+  renderMainBody() {
+    (<HTMLDivElement> document.getElementById("main-body")).textContent = "test"
+  }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 import { BodyRenderer } from "./common/body-renderer"
+
+const bodyRenderer = new BodyRenderer();
+bodyRenderer.renderMainBody();

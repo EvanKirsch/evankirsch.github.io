362f6ad854c32e460a4eac02f18b753ebb13a6cb
Evan Kirsch <40045743+EvanKirsch@users.noreply.github.com>
2026-02-19 21:24:42 +0000

    Make language widget a table

--- src/pages/language-widget.ts
+++ src/pages/language-widget.ts
@@ -16,17 +16,64 @@ export class LanguageWidget {
     const sortedLanguages = new Map([...myLanguages.entries()].sort((a, b) => b[1] - a[1])); // sort high to low
     let totalSize = 0; 
     myLanguages.values().forEach(elt => totalSize = totalSize + elt);
-    let widget = document.createElement("ol");
-    widget.classList.add("list-group");
-    widget.classList.add("list-group-flush")
+    let widget = document.createElement("table");
+    widget.classList.add("table")
+    let body = document.createElement("tbody");
 
+    widget.appendChild(this.buildHeaders());
+    widget.appendChild(body)
+
+    let c = 1; // counter for rank
     sortedLanguages.forEach((v, k) => {
-      let langEntry = document.createElement("li");
-      langEntry.classList.add("list-group-item")
-      langEntry.innerText = k + ": " + (100 * (v/totalSize)).toFixed(2).toString() + "%";
-      widget.appendChild(langEntry);
+      let langEntry = document.createElement("tr");
+
+      let th = document.createElement("th");
+      let tdLang = document.createElement("td");
+      let tdPerc = document.createElement("td");
+
+      th.scope = "row";
+      tdPerc.classList.add("text-right")
+
+      th.innerText = c.toString();
+      c++;
+      tdLang.innerText = k.toString();
+      tdPerc.innerText = (100 * (v/totalSize)).toFixed(2).toString() + "%";
+
+      langEntry.appendChild(th);
+      langEntry.appendChild(tdLang);
+      langEntry.appendChild(tdPerc);
+
+      body.appendChild(langEntry);
     });
+
     return widget;
+  }
+
+  private buildHeaders() : HTMLElement {
+    const thead = document.createElement("thead"); 
+    const tr = document.createElement("tr")
+
+    const rank = document.createElement("th");
+    const lang =  document.createElement("th");
+    const percent = document.createElement("th");
+
+    rank.scope = "col";
+    lang.scope = "col";
+    percent.scope = "col";
+
+    rank.innerText = "#";
+    lang.innerText = "Language";
+    percent.innerText = "Percent";
+
+    percent.classList.add("text-right")
+
+    tr.appendChild(rank);
+    tr.appendChild(lang);
+    tr.appendChild(percent);
+
+    thead.appendChild(tr);
+
+    return thead;
   }
 
   async _getElementById(id : string) : Promise<HTMLElement> {
